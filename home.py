d9f9785cd4386ffcd86da31aa6e52c360a28f555
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-02-07 11:36:43 +0530

    Minor fix

--- home.py
+++ home.py
@@ -9,8 +9,14 @@ import json
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 
-node = sx126x.sx126x(serial_num="/dev/ttyS0", freq=433, addr=30, power=22, rssi=False)
+# Get the device address from the user:
+print("Enter current device address (0-65535):")
+device_address = int(input())
 
+node = sx126x.sx126x(serial_num="/dev/ttyS0", freq=433, addr=device_address, power=22, rssi=False)
+
+print("Enter target node address (0-65535):")
+target_address = int(input())
 def send_command(command, target_address):
     """Sends a command."""
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -25,22 +31,18 @@ def send_command(command, target_address):
     time.sleep(0.2)
     print(f"Command sent to {target_address}.")
 
-
 try:
     time.sleep(1)
-    print("Enter target node address (0-65535):")
-    target_address = int(input())
-
     print("Press \033[1;32m1\033[0m to send Motor ON command")
     print("Press \033[1;32m2\033[0m to send Motor OFF command")
     print("Press \033[1;32m3\033[0m to send Motor STATUS request")
     print("Press \033[1;32mEsc\033[0m to exit")
 
     while True:
-        node.receive()  # Continuously try to receive
-        time.sleep(0.1)  # Small delay to prevent busy-waiting
+        node.receive()
+        time.sleep(0.1)
 
-        if select.select([sys.stdin],[],[], 0) == ([sys.stdin],[],): #check for command input
+        if select.select([sys.stdin],,, 0) == ([sys.stdin],,):
             c = sys.stdin.read(1)
 
             if c == '\x1b':  # Esc key
@@ -56,7 +58,7 @@ try:
             sys.stdout.flush()
 
 except ValueError:
-    print("Invalid target address. Please enter a number between 0 and 65535.")
+    print("Invalid address. Please enter a number between 0 and 65535.")
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
