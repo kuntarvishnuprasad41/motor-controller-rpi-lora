b52030dbacae9f9705c385935d209a8ffac1e699
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-02-07 12:40:10 +0530

    Sleep

--- target-always-rec.py
+++ target-always-rec.py
@@ -74,6 +74,7 @@ try:
                     elif command == "OFF":
                         GPIO.output(23, GPIO.LOW)   # Turn OFF relay 23
                         GPIO.output(24, GPIO.HIGH)  # Turn ON relay 24
+                        time.sleep(0.5)
                         GPIO.output(24, GPIO.LOW)  # Turn ON relay 24
                         send_reply("Motor off", target_address) # Send "Motor off" message
                     elif command == "STATUS":
